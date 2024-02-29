58d034fcc284e79f27d3b9c1ab028234fa192051
OlichkaKhamuliak <olkoboyko97@gmail.com>
2024-02-29 16:17:09 +0200

    added modal window when editing a contact.

--- src/components/ContactList/ContactItem.jsx
+++ src/components/ContactList/ContactItem.jsx
@@ -4,82 +4,52 @@ import { IoPersonRemove } from "react-icons/io5";
 import { IoPerson } from "react-icons/io5";
 import { FaPhone } from "react-icons/fa6";
 import { useDispatch } from "react-redux";
-import { deleteContact, updateContact } from "../../redux/operation";
+import { deleteContact } from "../../redux/operation";
 import { MdModeEditOutline } from "react-icons/md";
-import { GrUpdate } from "react-icons/gr";
+import EditContactModal from "../contactModal/contactModal";
 
-export const Contact = ({ contact: { name, phone, id } }) => {
+export const Contact = ({ contact }) => {
+  const { name, phone, id } = contact;
   const dispatch = useDispatch();
-
   const [isEditing, setIsEditing] = useState(false);
-  const [editedName, setEditedName] = useState(name);
-  const [editedPhone, setEditedPhone] = useState(phone);
 
-  const handleUpdateContact = () => {
-    if (editedName.trim() !== "" && editedPhone.trim() !== "") {
-      const updatedData = {
-        name: editedName,
-        phone: editedPhone,
-      };
-      dispatch(updateContact({ contactId: id, updatedData }));
-      setIsEditing(false); // Після оновлення закриваємо поля для редагування
-    }
+  const openEditModal = () => {
+    setIsEditing(true);
+  };
+
+  const closeEditModal = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className={css.wrap}>
       <div className={css.itemContent}>
-        {/* Показуємо редаговані поля тільки при натисканні на кнопку "Edit" */}
-        {!isEditing ? (
-          <>
-            <p className={css.text}>
-              <IoPerson className={css.icon} />
-              {name}
-            </p>
-            <p className={css.text}>
-              <FaPhone className={css.icon} />
-              {phone}
-            </p>
-          </>
-        ) : (
-          <>
-            <div className={css.editableField}>
-              <IoPerson className={css.icon} />
-              <input
-                type="text"
-                value={editedName}
-                onChange={(e) => setEditedName(e.target.value)}
-              />
-            </div>
-            <div className={css.editableField}>
-              <FaPhone className={css.icon} />
-              <input
-                type="tel"
-                value={editedPhone}
-                onChange={(e) => setEditedPhone(e.target.value)}
-              />
-            </div>
-          </>
-        )}
+        <p className={css.text}>
+          <IoPerson className={css.icon} />
+          {name}
+        </p>
+        <p className={css.text}>
+          <FaPhone className={css.icon} />
+          {phone}
+        </p>
       </div>
       <div>
-        {/* Показуємо кнопку "Edit" або "Update" залежно від стану редагування */}
-        {!isEditing ? (
-          <button className={css.btn} onClick={() => setIsEditing(true)}>
-            <MdModeEditOutline className={css.icon} size="25" />
-            Edit
-          </button>
-        ) : (
-          <button className={css.btn} onClick={handleUpdateContact}>
-            <GrUpdate className={css.icon} size="20" />
-            Update
-          </button>
-        )}
+        <button className={css.btn} onClick={openEditModal}>
+          <MdModeEditOutline className={css.icon} size="25" />
+          Edit
+        </button>
         <button className={css.btn} onClick={() => dispatch(deleteContact(id))}>
           <IoPersonRemove className={css.icon} size="25" />
           Delete
         </button>
       </div>
+      {isEditing && (
+        <EditContactModal
+          isOpen={isEditing}
+          closeModal={closeEditModal}
+          contact={contact}
+        />
+      )}
     </div>
   );
 };
